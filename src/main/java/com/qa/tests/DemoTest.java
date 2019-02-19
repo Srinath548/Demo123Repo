73fe28a189a76347bfbf8f74b3f8ccbb308a5c45
Shivva Srinath <ssrinath@corp.untd.com>
2019-02-19 11:43:20 +0530

    New changes.

--- src/main/java/com/qa/tests/DemoTest.java
+++ src/main/java/com/qa/tests/DemoTest.java
@@ -17,7 +17,7 @@ public class DemoTest {
 		int d=400;
 		int e=600;
 		int f=3300;
-		Assert.assertEquals(4000, a+b+c+d+e+f);		
+		Assert.assertEquals(5000, a+b+c+d+e+f);		
 		
 		System.out.println("...Test done....");
 	}
