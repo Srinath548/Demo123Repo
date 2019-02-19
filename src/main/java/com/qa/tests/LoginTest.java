b50421de63268f8c112a135d4086ddabc008bd2b
Shivva Srinath <ssrinath@corp.untd.com>
2019-02-19 12:18:30 +0530

    Adding new logic in the LoginPage.java

--- src/main/java/com/qa/tests/LoginTest.java
+++ src/main/java/com/qa/tests/LoginTest.java
@@ -5,8 +5,10 @@ public class LoginTest {
 	
 	public void loginPage()
 	{
-		System.out.println("New Login");
+		System.out.println("New Login feature.");
 		System.out.println("New Login page code");
+		System.out.println("Logged In Success.");
+		System.out.println("...................");
 	}
 	
 
